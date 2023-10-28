e04fa23be25a47c2615138e1db6a83b58dc5c40a
Prajwal Megalamani <prajwalmegalamani@changejar.in>
2023-10-28 19:21:06 +0530

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const corsConfig = {
+    origin: ['http://localhost:3001', 'https://git-users-fe.vercel.app/'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['*', 'Access-Control-Allow-Origin'],
+  };
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // You can also pass specific options here if needed
+  app.enableCors(corsConfig); // You can also pass specific options here if needed
   await app.listen(3000);
 }
 bootstrap();

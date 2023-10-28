41456aeb38ef40ad19ca34c264bfe7bf060a8788
Prajwal Megalamani <prajwalmegalamani@changejar.in>
2023-10-28 19:10:59 +0530

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // You can also pass specific options here if needed
   await app.listen(3000);
 }
 bootstrap();

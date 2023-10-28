fd4290454e170ee801e9c2c51d50853178ee9480
Prajwal Megalamani <prajwalmegalamani@changejar.in>
2023-10-28 18:51:18 +0530

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { NestFactory } from '@nestjs/core';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Disable CORS
-  app.enableCors({ origin: false });
-
   await app.listen(3000);
 }
 bootstrap();

e6e5bb6f93ccb3e04b8c1c12cb1fc93bb15ff2a5
Prajwal Megalamani <prajwalmegalamani@changejar.in>
2023-10-28 18:47:22 +0530

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,22 @@
-import {
-  Module,
-  NestModule,
-  MiddlewareConsumer,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NestFactory } from '@nestjs/core';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header(
-          'Access-Control-Allow-Headers',
-          'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-        );
-        if (req.method === 'OPTIONS') {
-          res.header('Access-Control-Allow-Methods', 'GET');
-          return res.status(200).json({});
-        }
-        next();
-      })
-      .forRoutes({ path: '*', method: RequestMethod.ALL });
-  }
+export class AppModule {}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Disable CORS
+  app.enableCors({ origin: false });
+
+  await app.listen(3000);
 }
+bootstrap();

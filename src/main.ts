37ef21758d518ee2e33e39faba8f81817afdaace
Prajwal Megalamani <prajwalmegalamani@changejar.in>
2023-10-30 13:50:08 +0530

    Cors 1

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use('/api', cors());
+  app.enableCors({
+    origin: 'https://git-users-fe.vercel.app/', // replace with your frontend domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept',
+  });
+  // other configurations
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(3000);
 }
+
 bootstrap();

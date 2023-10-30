f1360a65ee2b0f8d2348a973ad5c5469f6080a4f
Prajwal Megalamani <prajwalmegalamani@changejar.in>
2023-10-30 13:53:46 +0530

    Cors 1

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { NestFactory } from '@nestjs/core';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://git-users-fe.vercel.app/', // replace with your frontend domain
+    origin: 'https://git-users-fe-cros.vercel.app/', // replace with your frontend domain
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
